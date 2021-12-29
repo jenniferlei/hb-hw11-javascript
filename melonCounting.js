f3b9f259a7b0d0923cf0aedda022add8e46a5caa
Jennifer Lei <jenniferlei1928@gmail.com>
2021-12-29 23:35:29 +0000

    Completed melonCounting.js

---
+++ melonCounting.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// Our customers are going to buy lots of melons!
+
+const melonsToAdd = ['Ogen', 'Horned Melon', 'Watermelon', 'Casaba',
+                    'Sharlyn', 'Xigua', 'Ogen', 'Christmas', 'Christmas',
+                    'Christmas', 'Christmas', 'Watermelon', 'Sharlyn', 'Xigua',
+                    'Cantaloupe', 'Christmas', 'Watermelon', 'Christmas',
+                    'Sharlyn', 'Christmas', 'Cantaloupe', 'Casaba', 'Cantaloupe',
+                    'Santa Claus', 'Horned Melon', 'Watermelon', 'Ogen',
+                    'Horned Melon', 'Cantaloupe', 'Xigua', 'Horned Melon', 'Sharlyn',
+                    'Horned Melon', 'Sharlyn', 'Cantaloupe', 'Christmas',
+                    'Horned Melon', 'Horned Melon', 'Horned Melon', 'Xigua', 'Xigua',
+                    'Watermelon', 'Cantaloupe', 'Casaba', 'Cantaloupe', 'Casaba',
+                    'Watermelon', 'Santa Claus', 'Casaba']
+
+
+const countMelons = (melonArray) => {
+    // Take in an array and return a object of # of melons by melon type.
+
+    const melonCounts = {};
+
+    for (const melon of melonArray) {
+        if (melonCounts[melon]) {
+            melonCounts[melon] += 1;
+        } else {
+            melonCounts[melon] = 1;
+        }
+    }
+
+    return melonCounts;
+}
+
+console.log(countMelons(melonsToAdd));
